a93fd054afa0d53b43848cb6ded60d4699830fda
kentruri <kevinsteventrujillo@hotmail.com>
2024-09-16 19:51:27 -0500

    fix error

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const resolvers = {
     hello: (_, { message }) => {
         return `¡Hola, ${message}! Un saludo por parte del profe `;
       },
-    helloKevin: (_, { message }) => {
+    helloKev: (_, { message }) => {
       return `¡Hola, ${message}! Un saludo por parte de kevin `;
     },
   },
